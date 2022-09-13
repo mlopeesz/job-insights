06601cf2c79bea66abde65d023fcdd41a1152d06
Mauricio Lopes <mlopeesz@gmail.com>
2022-09-13 14:01:47 -0300

    feat: :sparkles: read function

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,7 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        jobs_reader = csv.reader(file, delimiter=",", quotechar='"')
+        job_list = [row for row in jobs_reader]
+        return job_list
