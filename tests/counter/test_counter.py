03e2a840781d5ed5006eb58d0cbf5778e239e6dd
Mauricio Lopes <mlopeesz@gmail.com>
2022-09-14 11:01:29 -0300

    test: :white_check_mark: count_ocurrences test

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    ocurrences_python = count_ocurrences("src/jobs.csv", "Python")
+    ocurrences_javascript = count_ocurrences("src/jobs.csv", "Javascript")
+    assert ocurrences_python == 1639
+    assert ocurrences_javascript == 122
